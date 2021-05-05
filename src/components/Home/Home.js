696db7f7ccee980de0c140e64eb52342a26b6b93
Restyled.io <commits@restyled.io>
2021-05-05 10:06:15 +0000

    Restyled by prettier

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,85 +12,90 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider,
 } from "@material-ui/pickers";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
-                               input : {
-                                 color : "#BB86FC",
-                               },
-                               textfield : {
-                                 color : "#BB86FC",
-                                 height : "50px",
-                                 width : "248px",
-                                 padding : "0px 0px 0px 0px",
-                                 margin : "-5px 0px 0px 0px",
-                               },
-                               paper : {
-                                 height : 70,
-                                 width : 250,
-                                 backgroundColor : "#31333F",
-                                 padding : "0px 10px 0px 10px",
-                               },
-                             }));
+  input: {
+    color: "#BB86FC",
+  },
+  textfield: {
+    color: "#BB86FC",
+    height: "50px",
+    width: "248px",
+    padding: "0px 0px 0px 0px",
+    margin: "-5px 0px 0px 0px",
+  },
+  paper: {
+    height: 70,
+    width: 250,
+    backgroundColor: "#31333F",
+    padding: "0px 10px 0px 10px",
+  },
+}));
 
-const Home =
-    () => {
-      const [state, setState] = useState([]);
-      const [selectedDate, setSelectedDate] = useState(new Date());
-      const [stateCode, setStateCode] = useState("States");
-      const [district, setDistricts] = useState([]);
-      const [districtCode, setDistrictCode] = useState("Districts");
-      const [vaccineData, setVaccineData] = useState([]);
-      const [formatedDate, setFormatedDate] = useState("");
-      const classes = useStyles();
+const Home = () => {
+  const [state, setState] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [stateCode, setStateCode] = useState("States");
+  const [district, setDistricts] = useState([]);
+  const [districtCode, setDistrictCode] = useState("Districts");
+  const [vaccineData, setVaccineData] = useState([]);
+  const [formatedDate, setFormatedDate] = useState("");
+  const classes = useStyles();
 
-      const a = JSON.stringify(vaccineData);
+  const a = JSON.stringify(vaccineData);
 
-      useEffect(() => {
-        setFormatedDate(selectedDate.toLocaleDateString().split("/").join("-"));
-      }, [ selectedDate, formatedDate ]);
+  useEffect(() => {
+    setFormatedDate(selectedDate.toLocaleDateString().split("/").join("-"));
+  }, [selectedDate, formatedDate]);
 
-      console.log(formatedDate, vaccineData);
+  console.log(formatedDate, vaccineData);
 
-      const handleDateChange = (date) => { setSelectedDate(date); };
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
-      useEffect(() => {
-        fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states")
-            .then((res) => res.json())
-            .then((data) => { setState(data.states); });
-      }, [ setState ]);
+  useEffect(() => {
+    fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states")
+      .then((res) => res.json())
+      .then((data) => {
+        setState(data.states);
+      });
+  }, [setState]);
 
-      const onStateChange = async (e) => {
-        const stateCode = e.target.value;
-        const url =
-            stateCode === "States"
-                ? "https://cdn-api.co-vin.in/api/v2/admin/location/districts/99"
-                : `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${
-                      stateCode}`;
-        await fetch(url).then((response) => response.json()).then((data) => {
-          setStateCode(stateCode);
-          setDistricts(data.districts);
-        });
-      };
+  const onStateChange = async (e) => {
+    const stateCode = e.target.value;
+    const url =
+      stateCode === "States"
+        ? "https://cdn-api.co-vin.in/api/v2/admin/location/districts/99"
+        : `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${stateCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setStateCode(stateCode);
+        setDistricts(data.districts);
+      });
+  };
 
-      const onDistrictChange = async (e) => {
-        const stateCode = e.target.value;
-        const url =
-            stateCode === "Districts"
-                ? null
-                : `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${
-                      stateCode}&date=${formatedDate}
+  const onDistrictChange = async (e) => {
+    const stateCode = e.target.value;
+    const url =
+      stateCode === "Districts"
+        ? null
+        : `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${stateCode}&date=${formatedDate}
         `;
-        await fetch(url).then((response) => response.json()).then((data) => {
-          setDistrictCode(stateCode);
-          setVaccineData(data);
-        });
-      };
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setDistrictCode(stateCode);
+        setVaccineData(data);
+      });
+  };
 
   return (
     <Container>
@@ -109,7 +114,7 @@ const Home =
               <Typography>Select State</Typography>
               <FormControl>
                 <Select
-  variant = "outlined"
+                  variant="outlined"
                   value={stateCode}
                   onChange={onStateChange}
                 >
@@ -118,15 +123,20 @@ const Home =
                     <MenuItem value={state?.state_id}>
                       {state?.state_name}
                     </MenuItem>
-                  ))
-    }</Select>
+                  ))}
+                </Select>
               </FormControl>
-          </div>
+            </div>
             <div className="text">
               <Typography>Select District</Typography>
-          <FormControl>< Select
-variant = "outlined"
-value = {districtCode} onChange = {onDistrictChange} > {district.map((state) => (
+              <FormControl>
+                <Select
+                  variant="outlined"
+                  value={districtCode}
+                  onChange={onDistrictChange}
+                >
+                  {" "}
+                  {district.map((state) => (
                     <MenuItem value={state?.district_id}>
                       {state?.district_name}
                     </MenuItem>
@@ -134,32 +144,36 @@ value = {districtCode} onChange = {onDistrictChange} > {district.map((state) => 
                 </Select>
               </FormControl>
             </div>
-            <div style={{
-    padding: "10px" }}>
+            <div
+              style={{
+                padding: "10px",
+              }}
+            >
               <Grid item xs={12} className="date__picker">
                 <Grid item>
                   <Paper className={classes.paper} elevation={3}>
                     <MuiPickersUtilsProvider utils={DateFnsUtils}>
                       <KeyboardDatePicker
-  margin = "normal"
-  id = "date-picker-dialog"
-  label = "Date picker dialog"
-  format = "dd-MM-yyyy"
-  value = {selectedDate} onChange = {handleDateChange} InputProps =
-  {
-    { className: classes.input }
-  } />
-                    </MuiPickersUtilsProvider >
-      </Paper>
-                </Grid><Grid item>
-      <Paper className = {classes.paper} elevation = {3}>< TextField
-  id = "outlined-number"
-  margin = "normal"
-  label = "Pin Code"
-  type = "number"
-  variant = "outlined"
-                      InputProps={
-    { className: classes.textfield, }}
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Date picker dialog"
+                        format="dd-MM-yyyy"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        InputProps={{ className: classes.input }}
+                      />
+                    </MuiPickersUtilsProvider>
+                  </Paper>
+                </Grid>
+                <Grid item>
+                  <Paper className={classes.paper} elevation={3}>
+                    <TextField
+                      id="outlined-number"
+                      margin="normal"
+                      label="Pin Code"
+                      type="number"
+                      variant="outlined"
+                      InputProps={{ className: classes.textfield }}
                     />
                   </Paper>
                 </Grid>
