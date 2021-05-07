bd559d2f7f6783d757a1d7bf42b51c6ce6ad2440
Restyled.io <commits@restyled.io>
2021-05-07 17:59:50 +0000

    Restyled by prettier

--- src/components/VaccineData/SingleVaccineData/VaccineDataSingle.js
+++ src/components/VaccineData/SingleVaccineData/VaccineDataSingle.js
@@ -1,32 +1,38 @@
-import "./VaccineDataSingle.css"
+import "./VaccineDataSingle.css";
 
-import Paper from '@material-ui/core/Paper';
-import {makeStyles} from '@material-ui/core/styles';
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-                               paperMainDiv : {width : "100%"},
-                             }));
+  paperMainDiv: { width: "100%" },
+}));
 
 const VaccineDataSingle = (vaccine) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return(
-        <div className={classes.paperMainDiv}>
-            <Paper variant="outlined" className="wrapper" style={{backgroundColor: "#333333"}}>
-                <div className="paper-left">
-                    <h1>{vaccine.name}</h1>
-                    <hr/>
-                    <br/>
-                    <div className="paper-left_address">
-                        <h3>address: </h3>
-                        <p>{vaccine.block_name}, {vaccine.district_name}, {vaccine.state_name}</p>
-                    </div>
-                </div>
-                <div className="paper-right">
-                </div>
-            </Paper>
+  return (
+    <div className={classes.paperMainDiv}>
+      <Paper
+        variant="outlined"
+        className="wrapper"
+        style={{ backgroundColor: "#333333" }}
+      >
+        <div className="paper-left">
+          <h1>{vaccine.name}</h1>
+          <hr />
+          <br />
+          <div className="paper-left_address">
+            <h3>address: </h3>
+            <p>
+              {vaccine.block_name}, {vaccine.district_name},{" "}
+              {vaccine.state_name}
+            </p>
+          </div>
         </div>
-    )
-}
+        <div className="paper-right"></div>
+      </Paper>
+    </div>
+  );
+};
 
-export default VaccineDataSingle
+export default VaccineDataSingle;
