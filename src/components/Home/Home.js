f6267b849a01202df4b0db75d6c5b80be638bc80
Restyled.io <commits@restyled.io>
2021-05-08 12:44:33 +0000

    Restyled by clang-format

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,110 +1,108 @@
 import "date-fns";
-import React, { useEffect, useState } from "react";
-import {
-  Grid,
-  Typography,
-  Container,
-  MenuItem,
-  FormControl,
-  Select,
-  Paper,
-  TextField,
-} from "@material-ui/core";
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from "@material-ui/pickers";
-import { makeStyles } from "@material-ui/core/styles";
+import "./Home.css";
+
 import DateFnsUtils from "@date-io/date-fns";
+import {
+  Container,
+  FormControl,
+  Grid,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import React, {useEffect, useState} from "react";
 
 import VaccineDataMain from "../VaccineData/VaccineDataMain";
 
-import "./Home.css";
-
 const useStyles = makeStyles((theme) => ({
-  input: {
-    color: "#bb86fc",
-  },
-  textfield: {
-    color: "#bb86fc",
-    height: "50px",
-    width: "248px",
-    padding: "0px 0px 0px 0px",
-    margin: "-5px 0px 0px 0px",
-  },
-  paper: {
-    height: 70,
-    width: 250,
-    backgroundColor: "#31333F",
-    padding: "0px 10px 0px 10px",
-  },
-}));
+                               input : {
+                                 color : "#bb86fc",
+                               },
+                               textfield : {
+                                 color : "#bb86fc",
+                                 height : "50px",
+                                 width : "248px",
+                                 padding : "0px 0px 0px 0px",
+                                 margin : "-5px 0px 0px 0px",
+                               },
+                               paper : {
+                                 height : 70,
+                                 width : 250,
+                                 backgroundColor : "#31333F",
+                                 padding : "0px 10px 0px 10px",
+                               },
+                             }));
 
-const Home = () => {
-  const [state, setState] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [stateCode, setStateCode] = useState("States");
-  const [district, setDistricts] = useState([]);
-  const [districtCode, setDistrictCode] = useState("Districts");
-  const [vaccineData, setVaccineData] = useState([]);
-  const [formattedDate, setFormattedDate] = useState("");
-  const classes = useStyles();
+const Home =
+    () => {
+      const [state, setState] = useState([]);
+      const [selectedDate, setSelectedDate] = useState(new Date());
+      const [stateCode, setStateCode] = useState("States");
+      const [district, setDistricts] = useState([]);
+      const [districtCode, setDistrictCode] = useState("Districts");
+      const [vaccineData, setVaccineData] = useState([]);
+      const [formattedDate, setFormattedDate] = useState("");
+      const classes = useStyles();
 
-  const GetFormattedDate = ()=> {
-    var month = selectedDate.getMonth() + 1;
-    var day = selectedDate.getDate();
-    var year = selectedDate.getFullYear();
-    var finalDate =  day + "-" + month + "-" + year;
+      const GetFormattedDate =
+          () => {
+            var month = selectedDate.getMonth() + 1;
+            var day = selectedDate.getDate();
+            var year = selectedDate.getFullYear();
+            var finalDate = day + "-" + month + "-" + year;
 
-    setFormattedDate(finalDate)
-  }
+            setFormattedDate(finalDate)
+          }
 
-  useEffect(() => {
-    GetFormattedDate()
-    // eslint-disable-next-line
-  }, [selectedDate, formattedDate]);
+      useEffect(
+          () => {
+              GetFormattedDate()
+              // eslint-disable-next-line
+          },
+          [ selectedDate, formattedDate ]);
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
+      const handleDateChange = (date) => { setSelectedDate(date); };
 
-  useEffect(() => {
-    fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states")
-      .then((res) => res.json())
-      .then((data) => {
-        setState(data.states)
-      });
-  }, [setState]);
+      useEffect(() => {
+        fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states")
+            .then((res) => res.json())
+            .then((data) => {setState(data.states)});
+      }, [ setState ]);
 
-  const onStateChange = async (e) => {
-    const stateCode = e.target.value;
-    const url =
-      stateCode === "States"
-        ? "https://cdn-api.co-vin.in/api/v2/admin/location/districts/9"
-        : `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${stateCode}`;
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setStateCode(stateCode);
-        setDistricts(data.districts);
-      });
-  };
+      const onStateChange = async (e) => {
+        const stateCode = e.target.value;
+        const url =
+            stateCode === "States"
+                ? "https://cdn-api.co-vin.in/api/v2/admin/location/districts/9"
+                : `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${
+                      stateCode}`;
+        await fetch(url).then((response) => response.json()).then((data) => {
+          setStateCode(stateCode);
+          setDistricts(data.districts);
+        });
+      };
 
-  const onDistrictChange = async (e) => {
-    const districtCode = e.target.value;
-    const url =
-      stateCode === "Districts"
-        ? null
-        : `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtCode}&date=${formattedDate}
+      const onDistrictChange = async (e) => {
+        const districtCode = e.target.value;
+        const url =
+            stateCode === "Districts"
+                ? null
+                : `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${
+                      districtCode}&date=${formattedDate}
         `;
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setDistrictCode(districtCode);
-        setVaccineData(data.sessions);
-        console.log(data.sessions)
-      });
-  };
+        await fetch(url).then((response) => response.json()).then((data) => {
+          setDistrictCode(districtCode);
+          setVaccineData(data.sessions);
+          console.log(data.sessions)
+        });
+      };
 
   return (
     <Container>
@@ -123,7 +121,7 @@ const Home = () => {
               <Typography>Select State</Typography>
               <FormControl>
                 <Select
-                  variant="outlined"
+  variant = "outlined"
                   value={stateCode}
                   onChange={onStateChange}
                 >
@@ -132,15 +130,14 @@ const Home = () => {
                     <MenuItem key={state?.state_id} value={state?.state_id}>
                       {state?.state_name}
                     </MenuItem>
-                  ))}
-                </Select>
+                  ))
+    }</Select>
               </FormControl>
-            </div>
+          </div>
             <div className="text">
               <Typography>Select District</Typography>
-              <FormControl>
-                <Select
-                  variant="outlined"
+          <FormControl>< Select
+variant = "outlined"
                   value={districtCode}
                   onChange={onDistrictChange}
                 >
@@ -149,10 +146,11 @@ const Home = () => {
                     <MenuItem key={district?.district_id} value={district?.district_id}>
                       {district?.district_name}
                     </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </div>
+                  ))
+                  }
+                  </Select>
+              </FormControl><
+                      /div>
             <div style={{ padding: "10px" }}>
               <Grid item xs={12} className="date__picker">
                 <Grid item>
@@ -167,9 +165,9 @@ const Home = () => {
                         onChange={handleDateChange}
                         InputProps={{ className: classes.input }}
                       />
-                    </MuiPickersUtilsProvider>
+                      </MuiPickersUtilsProvider>
                   </Paper>
-                </Grid>
+                      </Grid>
                 <Grid item>
                   <Paper className={classes.paper} elevation={3}>
                     <TextField
@@ -182,15 +180,14 @@ const Home = () => {
                         className: classes.textfield
                       }}
                     />
-                  </Paper>
-                </Grid>
-              </Grid>
+                      </Paper>
+                </Grid></Grid>
             </div>
-          </div>
-        </div>
-          <VaccineDataMain vaccineData={vaccineData}/>
-      </div>
-    </Container>
+                      </div>
+        </div><VaccineDataMain vaccineData =
+                                                {vaccineData} />
+      </div><
+                      /Container>
   );
 };
 
--- src/components/VaccineData/SingleVaccineData/VaccineDataSingle.js
+++ src/components/VaccineData/SingleVaccineData/VaccineDataSingle.js
@@ -1,87 +1,83 @@
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Badge from '@material-ui/core/Badge';
+import "./VaccineDataSingle.css"
 
+import Badge from '@material-ui/core/Badge';
+import Paper from '@material-ui/core/Paper';
+import {makeStyles} from '@material-ui/core/styles';
 import HealingIcon from '@material-ui/icons/Healing';
 import VerifiedUserRoundedIcon from '@material-ui/icons/VerifiedUserRounded';
 
-import "./VaccineDataSingle.css"
-
 const useStyles = makeStyles((theme) => ({
-    paperMainDiv:{
-        width: "100%"
-    },
-}));
+                               paperMainDiv : {width : "100%"},
+                             }));
 
-const VaccineDataSingle = (vaccine)=> {
-    const classes = useStyles();
+const VaccineDataSingle =
+    (vaccine) => {
+      const classes = useStyles();
 
-    return(
-        <div className={classes.paperMainDiv}>
-            <Paper variant="outlined" className="wrapper" style={{backgroundColor: "#333333"}}>
-                <div className="paper-left">
-                    <div>
-                        <h1>{vaccine.name} <VerifiedUserRoundedIcon style={{color: "#009E60"}}/></h1>
-                    </div>
-                    <hr/>
-                    <br/>
-                    <div className="paper-left_vaccine_head">
-                        <h3>Vaccine Name</h3>
+      return (
+          <div className = {classes.paperMainDiv}>
+          <Paper variant = "outlined" className =
+               "wrapper" style = {{ backgroundColor: "#333333" }}>
+          <div className = "paper-left"><div>
+          <h1>{vaccine.name}<VerifiedUserRoundedIcon style =
+                             {
+                               { color: "#009E60" }
+                             } /></h1></div>
+                    <hr/><br />
+          <div className = "paper-left_vaccine_head">
+          <h3>Vaccine Name</h3>
                         <p>{vaccine.vaccine}</p>
-                    </div>
+          </div>
 
                     <div className="paper-left_content">
                         <h3>address</h3>
-                        <p>{vaccine.block_name}, {vaccine.district_name}, {vaccine.state_name}</p>
+          <p>{vaccine.block_name},
+          {vaccine.district_name},
+          {vaccine.state_name}</p>
                     </div>
-                    <div className="paper-left_content">
-                        <h3>pincode</h3>
+          <div className = "paper-left_content">
+          <h3>pincode</h3>
                         <p>{vaccine.pincode}</p>
-                    </div>
+          </div>
                     <div className="paper-left_time">
                         <div className="open-time time">
                             Opening Time: {vaccine.from}
                         </div>
-                        <div className="close-time time">
-                            Closing Time: {vaccine.to}
-                        </div>
+          <div className = "close-time time">Closing Time:
+              {vaccine.to}</div>
                     </div>
-                </div>
+          </div>
                 <div className="paper-right">
                     <div className="paper-right_Badges">
                         <div className="paper-right_capacity">
                             <Badge color="secondary" badgeContent={vaccine.available_capacity}>
-                                <h5>Available Capacity</h5> <HealingIcon className="healing_icon"/>
-                            </Badge>
-                        </div>
-                        <div>
-                            <p><span>Date:</span>{vaccine.date}</p>
-                        </div>
+                                <h5>Available Capacity</h5>
+          <HealingIcon className = "healing_icon" />
+          </Badge>
+                        </div><div><p>
+          <span>Date: </span>{vaccine.date}</p></div>
                     </div>
-                    <div className="paper-right_age">
-                        <div className="age_text">
-                             <h3>Minimum Age</h3>
+          <div className = "paper-right_age"><div className = "age_text">
+          <h3>Minimum Age</h3>
                         </div>
-                        <div className="age_content">
-                            <p>{vaccine.min_age_limit} years</p>
+          <div className = "age_content">
+          <p>{vaccine.min_age_limit} years</p>
                         </div>
-                    </div>
+          </div>
                     <div className="paper-right_age">
                         <div className="age_text">
                              <h3>Minimum Fare(₹)</h3>
-                        </div>
+          </div>
                         <div className="age_content">
                             <p>{vaccine.fee_type} </p>
-                        </div>
-                    </div>
-                    <div className="paper-right_slots">
-                        <h3>Slots Available</h3>
+          </div>
+                    </div><div className = "paper-right_slots">
+          <h3>Slots Available<
+              /h3>
                         <p>{vaccine.slots.join(",")}</p>
-                    </div>
-                </div>
-            </Paper>
-        </div>
-    )
-}
+          </div>
+                </div></Paper>
+        </div>)
+    }
 
 export default VaccineDataSingle
