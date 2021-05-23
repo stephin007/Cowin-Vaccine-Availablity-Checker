05e3692935a9aa8878843a854629e57ab54b38fe
Restyled.io <commits@restyled.io>
2021-05-23 18:53:19 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React from "react";
 import "./App.css";
-import Main from "./components/Main";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from "./components/Home/Home";
-import Header from "./components/Header/Header";
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Main from "./components/Main";
 
 const App = () => {
-  return (
-    <Router>
-      <div className="app">
-        <Header />
-        <Switch>
-          <Route exact={true} path="/">
-            <Main />
+  return (<Router><div className = "app"><Header /><Switch>
+          <Route exact = {true} path = "/"><Main />
           </Route>
-          <Route path="/vaccines" exact={true}>
-            <Home />
-          </Route>
-          <Route path="/about" exact={true}>
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+          <Route path="/vaccines " exact={true}>
+           < Home /></Route>
+          <Route path="/about " exact={true}>
+                     < About /></Route>
+        </Switch></div>
+    </Router>);
 };
 
 export default App;
