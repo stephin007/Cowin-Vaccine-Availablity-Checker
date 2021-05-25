7620105b26164b82ab200a4ff357843e89eb6fa0
Yash <super.nashik@gmail.com>
2021-05-25 16:41:17 +0530

    Added Null State Component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
-import "date-fns";
-import React, { useEffect, useState } from "react";
+import 'date-fns';
+import React, { useEffect, useState } from 'react';
 import {
   Grid,
   Typography,
@@ -9,58 +9,58 @@ import {
   Select,
   Paper,
   TextField,
-} from "@material-ui/core";
+} from '@material-ui/core';
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
-} from "@material-ui/pickers";
-import { makeStyles } from "@material-ui/core/styles";
-import DateFnsUtils from "@date-io/date-fns";
+} from '@material-ui/pickers';
+import { makeStyles } from '@material-ui/core/styles';
+import DateFnsUtils from '@date-io/date-fns';
 
-import VaccineDataMain from "../VaccineData/VaccineDataMain";
+import VaccineDataMain from '../VaccineData/VaccineDataMain';
 
-import "./Home.css";
+import './Home.css';
 
 const useStyles = makeStyles((theme) => ({
   input: {
-    color: "#bb86fc",
+    color: '#bb86fc',
   },
   textfield: {
-    color: "#bb86fc",
-    height: "50px",
-    width: "248px",
-    padding: "0px 0px 0px 0px",
-    margin: "-5px 0px 0px 0px",
+    color: '#bb86fc',
+    height: '50px',
+    width: '248px',
+    padding: '0px 0px 0px 0px',
+    margin: '-5px 0px 0px 0px',
   },
   paper: {
     height: 70,
     width: 250,
-    backgroundColor: "#31333F",
-    padding: "0px 10px 0px 10px",
+    backgroundColor: '#31333F',
+    padding: '0px 10px 0px 10px',
   },
 }));
 
 const Home = () => {
   const [state, setState] = useState([]);
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [stateCode, setStateCode] = useState("States");
+  const [stateCode, setStateCode] = useState('States');
   const [district, setDistricts] = useState([]);
-  const [districtCode, setDistrictCode] = useState("Districts");
+  const [districtCode, setDistrictCode] = useState('Districts');
   const [vaccineData, setVaccineData] = useState([]);
-  const [formattedDate, setFormattedDate] = useState("");
+  const [formattedDate, setFormattedDate] = useState('');
   const classes = useStyles();
 
-  const GetFormattedDate = ()=> {
+  const GetFormattedDate = () => {
     var month = selectedDate.getMonth() + 1;
     var day = selectedDate.getDate();
     var year = selectedDate.getFullYear();
-    var finalDate =  day + "-" + month + "-" + year;
+    var finalDate = day + '-' + month + '-' + year;
 
-    setFormattedDate(finalDate)
-  }
+    setFormattedDate(finalDate);
+  };
 
   useEffect(() => {
-    GetFormattedDate()
+    GetFormattedDate();
     // eslint-disable-next-line
   }, [selectedDate, formattedDate]);
 
@@ -69,18 +69,18 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states")
+    fetch('https://cdn-api.co-vin.in/api/v2/admin/location/states')
       .then((res) => res.json())
       .then((data) => {
-        setState(data.states)
+        setState(data.states);
       });
   }, [setState]);
 
   const onStateChange = async (e) => {
     const stateCode = e.target.value;
     const url =
-      stateCode === "States"
-        ? "https://cdn-api.co-vin.in/api/v2/admin/location/districts/9"
+      stateCode === 'States'
+        ? 'https://cdn-api.co-vin.in/api/v2/admin/location/districts/9'
         : `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${stateCode}`;
     await fetch(url)
       .then((response) => response.json())
@@ -93,7 +93,7 @@ const Home = () => {
   const onDistrictChange = async (e) => {
     const districtCode = e.target.value;
     const url =
-      stateCode === "Districts"
+      stateCode === 'Districts'
         ? null
         : `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtCode}&date=${formattedDate}
         `;
@@ -102,32 +102,32 @@ const Home = () => {
       .then((data) => {
         setDistrictCode(districtCode);
         setVaccineData(data.sessions);
-        console.log(data.sessions)
+        console.log(data.sessions);
       });
   };
 
   return (
     <Container>
-      <div className="home">
+      <div className='home'>
         <h2>CoWIN Vaccination Slot Availability</h2>
-        <div className="home__info">
+        <div className='home__info'>
           <p>
             The CoWIN APIs are geo fenced, so sometimes you may not see an
             output! Please try after sometime
           </p>
         </div>
 
-        <div className="home__option">
-          <div className="home__optionRight">
-            <div className="text">
+        <div className='home__option'>
+          <div className='home__optionRight'>
+            <div className='text'>
               <Typography>Select State</Typography>
               <FormControl>
                 <Select
-                  variant="outlined"
+                  variant='outlined'
                   value={stateCode}
                   onChange={onStateChange}
                 >
-                  <MenuItem value="States">States</MenuItem>
+                  <MenuItem value='States'>States</MenuItem>
                   {state.map((state) => (
                     <MenuItem key={state?.state_id} value={state?.state_id}>
                       {state?.state_name}
@@ -136,33 +136,38 @@ const Home = () => {
                 </Select>
               </FormControl>
             </div>
-            <div className="text">
+            <div className='text'>
               <Typography>Select District</Typography>
               <FormControl>
                 <Select
-                  variant="outlined"
+                  variant='outlined'
                   value={districtCode}
                   onChange={onDistrictChange}
                 >
-                  <MenuItem value="States" disabled={true}>Select State First</MenuItem>
+                  <MenuItem value='States' disabled={true}>
+                    Select State First
+                  </MenuItem>
                   {district.map((district) => (
-                    <MenuItem key={district?.district_id} value={district?.district_id}>
+                    <MenuItem
+                      key={district?.district_id}
+                      value={district?.district_id}
+                    >
                       {district?.district_name}
                     </MenuItem>
                   ))}
                 </Select>
               </FormControl>
             </div>
-            <div style={{ padding: "10px" }}>
-              <Grid item xs={12} className="date__picker">
+            <div style={{ padding: '10px' }}>
+              <Grid item xs={12} className='date__picker'>
                 <Grid item>
                   <Paper className={classes.paper} elevation={3}>
                     <MuiPickersUtilsProvider utils={DateFnsUtils}>
                       <KeyboardDatePicker
-                        margin="normal"
-                        id="date-picker-dialog"
-                        label="Date picker dialog"
-                        format="dd-MM-yyyy"
+                        margin='normal'
+                        id='date-picker-dialog'
+                        label='Date picker dialog'
+                        format='dd-MM-yyyy'
                         value={selectedDate}
                         onChange={handleDateChange}
                         InputProps={{ className: classes.input }}
@@ -173,13 +178,13 @@ const Home = () => {
                 <Grid item>
                   <Paper className={classes.paper} elevation={3}>
                     <TextField
-                      id="outlined-number"
-                      margin="normal"
-                      label="Pin Code"
-                      type="number"
-                      variant="outlined"
+                      id='outlined-number'
+                      margin='normal'
+                      label='Pin Code'
+                      type='number'
+                      variant='outlined'
                       InputProps={{
-                        className: classes.textfield
+                        className: classes.textfield,
                       }}
                     />
                   </Paper>
@@ -188,7 +193,13 @@ const Home = () => {
             </div>
           </div>
         </div>
-          <VaccineDataMain vaccineData={vaccineData}/>
+        {districtCode === 'Districts' && stateCode === 'States' ? (
+          ''
+        ) : vaccineData.length > 0 ? (
+          <VaccineDataMain vaccineData={vaccineData} />
+        ) : (
+          'No results found for the entered State, Pin code or Date, Please Try Again'
+        )}
       </div>
     </Container>
   );
