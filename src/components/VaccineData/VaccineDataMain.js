666630fe4cb74c3306231acabb7edb6a76cea6e7
afif ahmed <afifahmed456123@gmail.com>
2021-05-28 19:44:18 +0530

    Revert "added a loading componetn"
    This reverts commit 59e921273ba23991291062dc3d407024f25ff469.

--- src/components/VaccineData/VaccineDataMain.js
+++ src/components/VaccineData/VaccineDataMain.js
@@ -1,29 +1,18 @@
-import VaccineDataSingle from './SingleVaccineData/VaccineDataSingle';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import VaccineDataSingle from "./SingleVaccineData/VaccineDataSingle";
 
-const VaccineDataMain = ({ vaccineData, loading }) => {
-	return (
-		<>
-			{loading === false ? (
-				<>
-					{vaccineData.map((vaccine) => {
-						const { center_id } = vaccine;
-						return <VaccineDataSingle key={center_id} {...vaccine} />;
-					})}
-				</>
-			) : (
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'center',
-						padding: '2rem 0',
-					}}
-				>
-					<CircularProgress />
-				</div>
-			)}
-		</>
-	);
+const VaccineDataMain = ({ vaccineData }) => {
+  return (
+    <>
+      {vaccineData.length !== 0 ? (
+        <>
+          {vaccineData.map((vaccine) => {
+            const { center_id } = vaccine;
+            return <VaccineDataSingle key={center_id} {...vaccine} />;
+          })}
+        </>
+      ) : null}
+    </>
+  );
 };
 
 export default VaccineDataMain;
