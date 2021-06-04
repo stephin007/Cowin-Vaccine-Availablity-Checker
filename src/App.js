5fee7639e54ff240075d8fb2ead530e10bf16377
stephin007 <stephinreji123@gmail.com>
2021-06-04 08:02:59 +0530

    added path to main app.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Intro from "./components/Intro/Intro";
+import CovidInfo from "./components/CovidInfo/CovidInfo";
 
 const App = () => {
   const [theme, toggleTheme] = useDarkMode();
@@ -37,6 +38,9 @@ const App = () => {
                 </Route>
                 <Route path="/vaccines" exact={true}>
                   <Home />
+                </Route>
+                <Route path="/covidinfo" exact={true}>
+                  <CovidInfo />
                 </Route>
                 <Route path="/about" exact={true}>
                   <About />
