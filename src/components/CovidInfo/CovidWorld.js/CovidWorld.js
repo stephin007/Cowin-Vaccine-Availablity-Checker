8d0ac4c8cae2917e05cbbdc2a94951e20261955e
stephin007 <stephinreji123@gmail.com>
2021-06-05 12:48:07 +0530

    Added paper for showing information

--- src/components/CovidInfo/CovidWorld.js/CovidWorld.js
+++ src/components/CovidInfo/CovidWorld.js/CovidWorld.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Paper } from "@material-ui/core";
+
+import "./CovidWorld.css";
 
 const CovidWorld = ({ value, index }) => {
-  return <>{value === index && <h1>World</h1>}</>;
+  return (
+    <>
+      {value === index && (
+        <>
+          <div class="world_wrapper">
+            <div class="world_head">
+              <Paper className="world_head_paper">Total Cases</Paper>
+              <Paper className="world_head_paper">Total Recovered</Paper>
+              <Paper className="world_head_paper">Total Vaccinated</Paper>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default CovidWorld;
