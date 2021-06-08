d56581ea3d13a1d33900504da2a02a405191523b
Kunal Kumar <kunakumar06022003@gmail.com>
2021-06-08 14:08:47 +0530

    handleTabChange test included

--- src/__tests__/covidInfoMain.js
+++ src/__tests__/covidInfoMain.js
@@ -4,7 +4,6 @@ import CovidInfo from '../components/CovidInfo/CovidInfoMain';
 
 describe('render covidInfoMain', () => {
 	const wrapper = shallow(<CovidInfo />);
-	console.log(wrapper.debug());
 	it('render covidinfo correctly', () => {
 		expect(wrapper).toMatchSnapshot();
 	});
@@ -15,13 +14,12 @@ describe('render covidInfoMain', () => {
 
 describe('check tab value change functionality', () => {
 	const wrapper = shallow(<CovidInfo />);
-	const event = {
-		target: { value: 'success' },
-	};
-	beforeEach(() => {
-		wrapper.find('[data-testId="covidinfo-tabValue"]').simulate('change', event);
-	});
-	it('change tab value', () => {
-		expect(wrapper.find('[data-testId="covidinfo-tabValue"]')).toBeCalledWith('success');
+	it('check handle tab change', () => {
+		const event = {
+			target: { value: 'success' },
+		};
+		const handleTabChange = wrapper.find('[data-testId="covidinfo-tabValue"]');
+		handleTabChange.simulate('change', event);
+		expect(wrapper).toMatchSnapshot();
 	});
 });
