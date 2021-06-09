ff9f1e9dc3f780a0a247dfaca1b72028c93327d3
himanshu <himanshujasuja1040@gmail.com>
2021-06-09 13:19:48 +0530

    Pagination and issue solved

--- src/components/VaccineData/VaccineDataMain.js
+++ src/components/VaccineData/VaccineDataMain.js
@@ -1,13 +1,59 @@
 import VaccineDataSingle from "./SingleVaccineData/VaccineDataSingle";
 
-const VaccineDataMain = ({ vaccineData }) => {
+const VaccineDataMain = ({
+  vaccineData,
+  filtervalueVaccine,
+  filtervalueAge,
+  filtervalueFare,
+}) => {
   return (
     <>
       {vaccineData?.map((vaccine) => {
-        return <VaccineDataSingle key={vaccine.center_id} {...vaccine} />;
+        return (
+          console.log(vaccine),
+          console.log(filtervalueVaccine, " > ", vaccine.vaccine),
+          console.log(parseInt(filtervalueAge), ">", vaccine.min_age_limit),
+          console.log(filtervalueFare, ">", vaccine.fee_type),
+          filtervalueVaccine === "ALL" &&
+          filtervalueAge === "ALL" &&
+          filtervalueFare === "ALL" ? (
+            (console.log("cond>>>"),
+            (<VaccineDataSingle key={vaccine.center_id} {...vaccine} />))
+          ) : filtervalueVaccine === vaccine.vaccine &&
+            filtervalueAge === "ALL" &&
+            filtervalueFare === "ALL" ? (
+            (console.log("cond1"),
+            (<VaccineDataSingle key={vaccine.center_id} {...vaccine} />))
+          ) : parseInt(filtervalueAge) === vaccine.min_age_limit &&
+            filtervalueVaccine === "ALL" &&
+            filtervalueFare === "ALL" ? (
+            (console.log("cond2"),
+            (<VaccineDataSingle key={vaccine.center_id} {...vaccine} />))
+          ) : parseInt(filtervalueFare) === vaccine.fee_type &&
+            filtervalueVaccine === "ALL" &&
+            filtervalueAge === "ALL" ? (
+            (console.log("cond3"),
+            (<VaccineDataSingle key={vaccine.center_id} {...vaccine} />))
+          ) : parseInt(filtervalueFare) === vaccine.fee_type &&
+            filtervalueVaccine === vaccine.vaccine &&
+            filtervalueAge === "ALL" ? (
+            <VaccineDataSingle key={vaccine.center_id} {...vaccine} />
+          ) : filtervalueFare === vaccine.fee_type &&
+            parseInt(filtervalueAge) === vaccine.min_age_limit &&
+            filtervalueVaccine === "ALL" ? (
+            <VaccineDataSingle key={vaccine.center_id} {...vaccine} />
+          ) : filtervalueVaccine === vaccine.vaccine &&
+            parseInt(filtervalueAge) === vaccine.min_age_limit &&
+            filtervalueFare === "ALL" ? (
+            <VaccineDataSingle key={vaccine.center_id} {...vaccine} />
+          ) : filtervalueVaccine === vaccine.vaccine &&
+            parseInt(filtervalueAge) === vaccine.min_age_limit &&
+            filtervalueFare === vaccine.fee_type ? (
+            <VaccineDataSingle key={vaccine.center_id} {...vaccine} />
+          ) : null
+        );
       })}
     </>
   );
 };
-
 export default VaccineDataMain;
